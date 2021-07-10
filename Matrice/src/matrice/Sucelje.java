558acbd0e3682ed65b287d6264d1fdcbe7d0a8db
Martina Gaćina <margaci.math@pmf.hr>
2021-07-10 15:11:01 +0200

    sucelje za matrice, metode plus, minus, puta i rang

---
+++ Matrice/src/matrice/Sucelje.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrice;
+
+/**
+ *
+ * @author Martina
+ */
+public interface Sucelje {
+    public int rang();
+    public Matrica plus(Matrica M);
+    public Matrica minus(Matrica M);
+    public Matrica puta(Matrica M) throws IllegalArgumentException, InterruptedException;
+}
